32635cfa4a8bfc7dce3b6234374bd740d709ce7c
eltonholmes1980 <eltonholmes1980@gmail.com>
2020-04-12 12:29:35 -0400

    Easter v

--- main.R
+++ main.R
@@ -19,4 +19,23 @@ train$Embarked[train$Embarked ==""]  <- "S"
 summary(train$Age)
 train$MissingAge <- ifelse(is.na(train$Age), "Y", "N")
 
+## Calculate family size
+train$FamilySize <- 1 + train$SibSp + train$Parch
+
+train$Survived <- as.factor(train$Survived)
+train$Pclass <- as.factor(train$Pclass)
+train$Sex <- as.factor(train$Sex)
+train$Embarked <- as.factor(train$Embarked)
+train$MissingAge <- as.factor(train$MissingAge)
+
+features <- c("Survived", "Pclass", "Sex", "Age","SibSp", "Parch", 
+              "Fare", "Embarked", "MissingAge", "FamilySize")
+train <- train[,features]
+str(train)
+
+
+
+
+
+
 
